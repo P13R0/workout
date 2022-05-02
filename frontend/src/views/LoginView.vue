fe56876a0065c05fa54019fbfe108f2de78f3523
Piero Anesin <piero.anesin@cherrychain.it>
2022-05-02 22:42:50 +0200

    init login view ui

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,15 +1,44 @@
 <template>
-  <div class="login">
-    <h1>This is the login page</h1>
-  </div>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>Workout Manager</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+          <form @submit.prevent="handleLogin">
+            <ion-item>
+              <ion-label position="floating">Username</ion-label>
+              <ion-input v-model="username" required></ion-input>
+            </ion-item>
+            <ion-item>
+              <ion-label position="floating">Password</ion-label>
+              <ion-input v-model="password" type="password" required></ion-input>
+            </ion-item>
+            <ion-button type="submit" shape="round">Login</ion-button>
+          </form>
+        </ion-card-content>
+      </ion-card>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
+import {IonPage, IonCard, IonInput, IonLabel, IonItem, IonButton} from '@ionic/vue'
+
 export default {
-  name: "LoginView"
+  name: "LoginView",
+  components: {IonPage, IonCard, IonInput, IonLabel, IonItem, IonButton},
+  data: () => ({
+    username: "",
+    password: ""
+  }),
+  methods: {}
 }
 </script>
 
 <style scoped>
-
+ion-button {
+  margin-top: 2em;
+}
 </style>
