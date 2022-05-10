65de3ca420f1821caef6801e6d2ba9d7f8a42722
P13R0 <pieroanesin@gmail.com>
2022-05-10 11:40:58 +0200

    import all ionic css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,22 @@ import { IonicVue } from '@ionic/vue';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker'
-import '@ionic/core/css/ionic.bundle.css'
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
 
 import { IonPage, IonHeader, IonContent } from '@ionic/vue'
 import { IonCard, IonCardTitle, IonCardHeader, IonCardContent } from '@ionic/vue'
